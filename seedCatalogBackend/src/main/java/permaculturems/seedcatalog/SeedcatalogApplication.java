8f631dc2a8135e2a8ed3316056491032323b182d
djh007ktr <djhill0531@gmail.com>
2023-07-24 18:11:15 -0500

    Initialize spring boot backend with mongodb

---
+++ seedCatalogBackend/src/main/java/permaculturems/seedcatalog/SeedcatalogApplication.java
@@ -0,0 +1,13 @@
+package permaculturems.seedcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeedcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeedcatalogApplication.class, args);
+	}
+
+}
---
+++ seedCatalogBackend/src/test/java/permaculturems/seedcatalog/SeedcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package permaculturems.seedcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeedcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
